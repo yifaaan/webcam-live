3581d1ba23bb97588bddbe46dcbd7830a7568cf9
Liuyifan <0219lyf@gmail.com>
2025-04-19 23:14:23 +0800

    does not work

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,50 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use js_sys::wasm_bindgen::JsValue;
+use sycamore::{
+    prelude::MaybeDyn,
+    web::{
+        GlobalProps, NodeRef, SetAttribute,
+        tags::{HtmlVideo, video},
+    },
+};
+use tracing::info;
+use wasm_bindgen::JsCast;
+
+pub struct VideoStream {
+    el: HtmlVideo,
+}
+
+impl VideoStream {
+    pub fn new(node_ref: NodeRef) -> Self {
+        let el = video();
+        Self {
+            el: el.r#ref(node_ref),
+        }
+    }
+
+    pub async fn set_video_src(&mut self, video_constraints: &serde_json::Value) {
+        let window = web_sys::window().expect("no global window");
+        let navigator = window.navigator();
+        let media_devices = navigator.media_devices().expect("no media devices");
+        let mut constraints = web_sys::MediaStreamConstraints::new();
+        constraints.set_video(&JsValue::from_bool(true));
+        info!("devices(tracing_wasm): {:?}", media_devices);
+        web_sys::console::log_1(&media_devices);
+        let mut constraints = web_sys::MediaStreamConstraints::new();
+        // web_sys::console::log_1(&constraints);
+        let json_string = serde_json::to_string(video_constraints).unwrap();
+        let js_value = js_sys::JSON::parse(&json_string).unwrap();
+        constraints.set_video(&js_value);
+        constraints.set_audio(&false.into());
+        let media = wasm_bindgen_futures::JsFuture::from(
+            media_devices
+                .get_user_media_with_constraints(&constraints)
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+        // let media_stream = media.unchecked_into::<web_sys::MediaStream>();
+        // info!("media_stream(tracing_wasm): {:?}", media_stream);
+        let a = MaybeDyn::from(media);
+        self.el.set_attribute("srcObject", a);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use sycamore::prelude::*;
-
+use sycamore::web::tags::HtmlVideo;
+use sycamore_futures::spawn_local_scoped;
+use webcam_live::VideoStream;
 #[derive(Props)]
 struct HelloProps {
     name: String,
@@ -13,17 +15,43 @@ fn Greeting(name: HelloProps) -> View {
 }
 
 #[component]
-fn App() -> View {
+fn Video() -> View {
+    let video_ref = create_node_ref();
+    spawn_local_scoped(async move {
+        let mut video_stream = VideoStream::new(video_ref);
+        video_stream
+            .set_video_src(&serde_json::json!({
+                "audio": false,
+                "video": {
+                    "facingMode": "environment",
+                    "width": 1280,
+                    "height": 720,
+                }
+            }))
+            .await;
+    });
+
     view! {
-        div {
-            h1 { "Sycamore 入门" }
-            Greeting(name="lyf".into())
-        }
+        div(class="flex justify-center") {
+           video(
+                ref=video_ref,
+                class="border-2 border-gray-300 rounded-md",
+                autoplay=true,
+                width="1280",
+                height="720",
+            )
+       }
     }
 }
 fn main() {
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
 
-    sycamore::render(App);
+    sycamore::render(|| {
+        view! {
+            div(class="container p-2") {
+                Video()
+            }
+        }
+    });
 }
